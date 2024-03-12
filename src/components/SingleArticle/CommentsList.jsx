39c6490661956e187b53f95c3c24872a90e029b7
edthuman <edwardadirvine@aol.com>
2024-03-12 10:50:05 +0000

    add Loading component into CommentsList

--- src/components/SingleArticle/CommentsList.jsx
+++ src/components/SingleArticle/CommentsList.jsx
@@ -2,25 +2,27 @@ import { useEffect, useState } from "react";
 import { getCommentsFromArticleID } from "../../api";
 import SingleComment from "./SingleComment";
 import "./CommentsList.css"
+import Loading from "../Loading/Loading";
 
 function CommentsList(article_id) {
     const [comments, setComments] = useState([]);
+    const [areCommentsLoading, setAreCommentsLoading] = useState(true)
 
     useEffect(() => {
         getCommentsFromArticleID(article_id).then((returnedComments) => {
             setComments(returnedComments);
+            setAreCommentsLoading(false)
         });
     }, []);
 
-    return (
-        <section id="comment-list">
+    return (areCommentsLoading ? <Loading/> :
+        (<section id="comment-list">
             {comments.map((comment) => {
-                console.log(comment);
                 return (
                     <SingleComment key={comment.comment_id} comment={comment} />
                 );
             })}
-        </section>
+        </section>)
     );
 }
 
