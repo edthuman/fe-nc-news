622a4089e6330716b4f71579bcfca87ee35e492d
edthuman <edwardadirvine@aol.com>
2024-03-12 12:29:07 +0000

    update link in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom"
-import "./Header.css"
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
     return (
         <h1>
-            <Link to="./" id="header">NC News</Link>
+            <Link to="/" id="header">
+                NC News
+            </Link>
         </h1>
-    )
+    );
 }
 
-export default Header
+export default Header;
