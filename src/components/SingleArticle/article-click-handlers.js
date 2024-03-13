abcefb5744bb4da7aad4519633c3cc9f0626cf9e
edthuman <edwardadirvine@aol.com>
2024-03-13 11:55:50 +0000

    add comment post error handling

--- src/components/SingleArticle/article-click-handlers.js
+++ src/components/SingleArticle/article-click-handlers.js
@@ -38,20 +38,31 @@ function handleCommentPost(
     article_id,
     username,
     comment,
-    setComments
+    setComments,
+    setIsAddCommentClicked,
+    setIsSubmissionError
 ) {
     e.preventDefault();
+    setIsSubmissionError(false);
     setIsCommentSubmitted(true);
     if (comment.length) {
-        setIsSubmitLoading(true)
-        postComment(article_id, username, comment).then((response) => {
-            setComments((currComments) => {
-                return [response, ...currComments];
+        setIsSubmitLoading(true);
+        postComment(article_id, username, comment)
+            .then((response) => {
+                setComments((currComments) => {
+                    return [response, ...currComments];
+                });
+                setIsSubmitLoading(false);
+                setCommentInput("");
+                setIsCommentSubmitted(false);
+                setIsAddCommentClicked(false);
+                setIsSubmissionError(false);
+            })
+            .catch((err) => {
+                setIsSubmissionError(true);
+                setIsSubmitLoading(false);
+                setIsCommentSubmitted(false);
             });
-            setIsSubmitLoading(false);
-            setCommentInput("");
-            setIsCommentSubmitted(false);
-        });
     }
 }
 
