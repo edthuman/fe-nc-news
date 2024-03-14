c5e1562c2b8cae555cd85e7058c7641a032c52c6
edthuman <edwardadirvine@aol.com>
2024-03-14 16:00:47 +0000

    change p tag to h2 in RouteNotFound component

--- src/components/RouteNotFound/RouteNotFound.jsx
+++ src/components/RouteNotFound/RouteNotFound.jsx
@@ -4,7 +4,7 @@ import "./RouteNotFound.css";
 function RouteNotFound() {
     return (
         <div id="page-not-found">
-            <p>We couldn't find the page you were looking for</p>
+            <h2>We couldn't find the page you were looking for</h2>
             <Link to="/">{`<<To front page`}</Link>
         </div>
     );
